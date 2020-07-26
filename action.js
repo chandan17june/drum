70ad66eb2ea29a4cbe9b7c42c904b75851fc31a7
chandan17june <60036753+chandan17june@users.noreply.github.com>
2020-07-26 21:05:13 +0530

    Add files via upload

---
+++ action.js
@@ -0,0 +1,59 @@
+//Detection button press
+var numberOfDrumButton = document.querySelectorAll(".drum").lenght;
+
+for (var i = 0; i < 7; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+    var buttonText = this.innerHTML;
+    makeSound(buttonText);         // sound
+    buttonAnimation(buttonText);   // animation
+  });
+}
+
+//Detection keyboard press
+document.addEventListener("keypress", function(event) {
+  makeSound(event.key);
+  buttonAnimation(event.key);
+})
+
+function makeSound(key) {
+  switch (key) {
+    case "w":
+      var audio = new Audio("sounds/tom-1.mp3");
+      audio.play();
+      break;
+    case "a":
+      var audio = new Audio("sounds/tom-2.mp3");
+      audio.play();
+      break;
+    case "s":
+      var audio = new Audio("sounds/tom-3.mp3");
+      audio.play();
+      break;
+    case "d":
+      var audio = new Audio("sounds/tom-4.mp3");
+      audio.play();
+      break;
+    case "j":
+      var audio = new Audio("sounds/snare.mp3");
+      audio.play();
+      break;
+    case "k":
+      var audio = new Audio("sounds/crash.mp3");
+      audio.play();
+      break;
+    case "l":
+      var audio = new Audio("sounds/kick-bass.mp3");
+      audio.play();
+      break;
+    default:
+  }
+}
+
+function buttonAnimation(currentKey) {
+  var activebutton = document.querySelector("." + currentKey);
+  activebutton.classList.add("pressed");
+
+  setTimeout(function() {
+    activebutton.classList.remove("pressed");
+  }, 100);
+}
